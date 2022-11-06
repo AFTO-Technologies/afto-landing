80048e339650bb9d27effb478f245adb8f559456
kaushik-codemonkey <kaushiktechie03@gmail.com>
2022-11-06 19:38:35 +0530

    Modify Header component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,54 @@
 import styles from './Header.module.scss'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+export const Header = () => {
+    return <div className={`${styles.header}`}>
+        <div className={`${styles['text-logo']}`}>
+            Afto
+        </div>
+        <NavBar/>
+    </div>
+}
 
-export const Header = ()=>{
-    return <div className={`${styles.logo}`}>
-        AFTO
+const NavBar = ()=>{
+    const router = useRouter();
+    return <div className={`${styles.navbar}`}>
+        <div className={`${styles.sidebar}`}>
+            {/* <i */}
+
+        </div>
+        <div className={`${styles.horizontalbar}`}>
+
+                <li className={`${router.pathname == "/" ? "active" : ""}`}>
+                <Link style={{ padding: "10px 15px" }} href="/" >
+                Home
+              </Link>
+            </li>
+            <li className={`${router.pathname == "#services" ? "active" : ""}`}>
+              <Link style={{ padding: "10px 15px" }} href="#services">
+                Services
+              </Link>
+            </li>
+            <li className={`${router.pathname == "#projects" ? "active" : ""}`}>
+                <Link style={{ padding: "10px 15px" }} href="#projects">
+                Projects
+              </Link>
+            </li>
+            <li className={`${router.pathname == "#blog" ? "active" : ""}`}>
+              <Link style={{ padding: "10px 15px" }} href="#blog">
+                Blog
+              </Link>
+            </li>
+            <li className={`${router.pathname == "#pricing" ? "active" : ""}`}>
+              <Link style={{ padding: "10px 15px" }} href="#pricing">
+                Pricing
+              </Link>
+            </li>
+            <li className={`${router.pathname == "#contact" ? "active" : ""}`}>
+              <Link style={{ padding: "10px 15px" }} href="#contact">
+                Contact
+              </Link>
+            </li>
+            </div>
     </div>
 }
---
+++ components/product-statement/ProductStatement.tsx
@@ -0,0 +1,9 @@
+import styles from './ProductStatement.module.scss'
+
+export const ProductStatement = () => {
+    return <div className={`${styles.productStatement}`}>
+        <div className={`${styles.productDefn}`}>
+        “Afto” is a mobile app that connects Event-Planners  with potential customers. Planners can use “Afto” to create and promote their Events, and connect with consumers who are interested in them. “Afto”, also provides a platform for consumers to find the best Planners for their requirements.
+        </div>
+    </div>
+}
